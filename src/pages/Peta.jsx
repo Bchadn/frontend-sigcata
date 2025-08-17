627b6cb3b518d5643d4163f34de230375e8ecf6e
Bchadn <bachtiaradino@students.undip.ac.id>
2025-08-17 15:11:35 +0700

    Tambah Navigasi Cesium

--- src/pages/Peta.jsx
+++ src/pages/Peta.jsx
@@ -2,9 +2,12 @@
 import React, { useEffect, useState, useRef, useCallback } from 'react';
 import { Viewer, GeoJsonDataSource } from 'resium';
 import * as Cesium from 'cesium';
+import CesiumNavigation from 'cesium-navigation-es6';
 import 'cesium/Build/Cesium/Widgets/widgets.css';
+import 'cesium-navigation-es6/dist/styles/cesium-navigation.css'
 import '../styles/StylePeta.css';
 import '../styles/StyleApp.css';
+
 
 // Import komponen yang diperlukan
 import LayerControl from '../components/LayerControl';
